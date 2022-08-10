3efacd6ab2c0a2bf7b922243212c1cefaba6dc6e
mr-white <50288473+khinemyaezin@users.noreply.github.com>
2022-08-10 22:17:42 +0630

    v 3

--- js/main.js
+++ js/main.js
@@ -1,119 +1,115 @@
- AOS.init({
- 	duration: 800,
- 	easing: 'slide'
- });
+AOS.init({
+  duration: 800,
+  easing: "slide",
+  once: false,
+  startEvent: 'DOMContentLoaded',
+});
 
-(function($) {
+(function ($) {
+  // Menu
+  var menu = function () {
+    $("body").on("click", ".js-nav-toggle", function (event) {
+      event.preventDefault();
 
-   // Menu
-	var menu = function() {
+      if ($("#ftco-nav").is(":visible")) {
+        $(this).removeClass("active");
+      } else {
+        $(this).addClass("active");
+      }
+    });
+  };
+  menu();
 
-		$('body').on('click', '.js-nav-toggle', function(event){
+  // scroll
+  var scrollWindow = function () {
+    $(window).scroll(function () {
+      var $w = $(this),
+        st = $w.scrollTop(),
+        navbar = $(".ftco_navbar"),
+        sd = $(".js-scroll-wrap");
 
-			event.preventDefault();
-
-			if ( $('#ftco-nav').is(':visible') ) {
-				$(this).removeClass('active');
-			} else {
-				$(this).addClass('active');	
-			}
-		});
-
-	};
-	menu();
-
-	// scroll
-	var scrollWindow = function() {
-		$(window).scroll(function(){
-			var $w = $(this),
-					st = $w.scrollTop(),
-					navbar = $('.ftco_navbar'),
-					sd = $('.js-scroll-wrap');
-
-			if (st > 150) {
-				if ( !navbar.hasClass('scrolled') ) {
-					navbar.addClass('scrolled');	
-				}
-			} 
-			if (st < 150) {
-				if ( navbar.hasClass('scrolled') ) {
-					navbar.removeClass('scrolled sleep');
-				}
-			} 
-			if ( st > 350 ) {
-				if ( !navbar.hasClass('awake') ) {
-					navbar.addClass('awake');	
-				}
-				
-				if(sd.length > 0) {
-					sd.addClass('sleep');
-				}
-			}
-			if ( st < 350 ) {
-				if ( navbar.hasClass('awake') ) {
-					navbar.removeClass('awake');
-					navbar.addClass('sleep');
-				}
-				if(sd.length > 0) {
-					sd.removeClass('sleep');
-				}
-			}
-		});
-	};
-	scrollWindow();
-
-
-	var onePageClick = function() {
-
-		$(document).on('click', '#ftco-nav a[href^="#"]', function (event) {
-	    event.preventDefault();
-
-	    var href = $.attr(this, 'href');
-
-	    $('html, body').animate({
-	        scrollTop: $($.attr(this, 'href')).offset().top - 70
-	    }, 500, function() {
-	    	// window.location.hash = href;
-	    });
-		});
-
-	};
-	onePageClick();
-
-
-	var textTypeV2 = function(destination, data) {
-        var aText = data;
-        var iSpeed = 150; // time delay of print out
-        var iIndex = 0; // start printing array at this posision
-        var iArrLength = aText[0].length; // the length of the text array
-        var iScrollAt = 20; // start scrolling up at this many lines
-
-        var iTextPos = 0; // initialise text position
-        var sContents = ""; // initialise contents variable
-        var iRow; // initialise current row
-
-        function typewriter() {
-            sContents = " ";
-            iRow = Math.max(0, iIndex - iScrollAt);
-            while (iRow < iIndex) {
-                sContents += aText[iRow++] + "<br />";
-            }
-            destination.innerHTML =
-                sContents + aText[iIndex].substring(0, iTextPos) + "";
-            if (iTextPos++ == iArrLength) {
-                iTextPos = 0;
-                iIndex++;
-                if (iIndex != aText.length) {
-                    iArrLength = aText[iIndex].length;
-                    setTimeout(typewriter, 500);
-                }
-            } else {
-                setTimeout(typewriter, iSpeed);
-            }
+      if (st > 150) {
+        if (!navbar.hasClass("scrolled")) {
+          navbar.addClass("scrolled");
         }
-        typewriter();
+      }
+      if (st < 150) {
+        if (navbar.hasClass("scrolled")) {
+          navbar.removeClass("scrolled sleep");
+        }
+      }
+      if (st > 350) {
+        if (!navbar.hasClass("awake")) {
+          navbar.addClass("awake");
+        }
+
+        if (sd.length > 0) {
+          sd.addClass("sleep");
+        }
+      }
+      if (st < 350) {
+        if (navbar.hasClass("awake")) {
+          navbar.removeClass("awake");
+          navbar.addClass("sleep");
+        }
+        if (sd.length > 0) {
+          sd.removeClass("sleep");
+        }
+      }
+    });
+  };
+  scrollWindow();
+
+  var onePageClick = function () {
+    $(document).on("click", '#ftco-nav a[href^="#"]', function (event) {
+      event.preventDefault();
+
+      var href = $.attr(this, "href");
+
+      $("html, body").animate(
+        {
+          scrollTop: $($.attr(this, "href")).offset().top - 70,
+        },
+        500,
+        function () {
+          // window.location.hash = href;
+        }
+      );
+    });
+  };
+  onePageClick();
+
+  var textTypeV2 = function (destination, data) {
+    var aText = data;
+    var iSpeed = 150; // time delay of print out
+    var iIndex = 0; // start printing array at this posision
+    var iArrLength = aText[0].length; // the length of the text array
+    var iScrollAt = 20; // start scrolling up at this many lines
+
+    var iTextPos = 0; // initialise text position
+    var sContents = ""; // initialise contents variable
+    var iRow; // initialise current row
+
+    function typewriter() {
+      sContents = " ";
+      iRow = Math.max(0, iIndex - iScrollAt);
+      while (iRow < iIndex) {
+        sContents += aText[iRow++] + "<br />";
+      }
+      destination.innerHTML =
+        sContents + aText[iIndex].substring(0, iTextPos) + "";
+      if (iTextPos++ == iArrLength) {
+        iTextPos = 0;
+        iIndex++;
+        if (iIndex != aText.length) {
+          iArrLength = aText[iIndex].length;
+          setTimeout(typewriter, 500);
+        }
+      } else {
+        setTimeout(typewriter, iSpeed);
+      }
     }
-	textTypeV2(document.querySelector(".typewrite-v2"),['I build things.....']);
-
+    typewriter();
+  };
+  textTypeV2(document.querySelector(".typewrite-v2"), ["I build crazy things....."]);
 })(jQuery);
-
