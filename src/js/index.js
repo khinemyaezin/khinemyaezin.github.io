971f97e9ea76ab361f9ba613fa3dfdd388a46fb0
Khine Myae <50288473+khinemyaezin@users.noreply.github.com>
2024-03-18 13:33:07 +0630

    update work experiences

--- src/js/index.js
+++ src/js/index.js
@@ -9,124 +9,108 @@ import SpaceAnimation from "./space";
 class Portfolio {
   constructor() {
     this.projects = [
-        {
-          title: "Featured Project",
-          subtitle: "Cable Locator",
-          desc: "Cable Detection Service landing website with ability to submit quote requests and control by admin page.",
-          published_year: "Nov 2022",
-          languages: ["Javascript with Jquery", "Firebase", "Bootstrap"],
-          links: [
-            {
-              url: "https://cablelocator.net",
-              icon: "box-arrow-up-right",
-            },
-          ],
-          imgSrc: "assets/cablelocator.png",
-        },
-        {
-          title: "Featured Project",
-          subtitle: "Business website",
-          desc: "Website using Laravel, pure js as front-end with Bootstrap. Provide with admin dashboard and customizable themes.",
-          published_year: "2022",
-          languages: ["Laravel", "Jquery", "Bootstrap"],
-          links: [
-            {
-              url: "https://github.com/khinemyaezin/Jupitor",
-              icon: "github",
-            },
-            {
-              url: "https://khinemyaezin.com/Jupitor/",
-              icon: "box-arrow-up-right",
-            },
-          ],
-          imgSrc: "assets/business-website.png",
-        },
-        {
-          title: "Featured Project",
-          subtitle: "Ecommerce",
-          desc: `All in one place to create <span class="highlight">beautiful online stores</span>
+      {
+        title: "Featured Project",
+        subtitle: "Cable Locator",
+        desc: "Cable Detection Service landing website with ability to submit quote requests and control by admin page.",
+        published_year: "Nov 2022",
+        languages: ["Javascript with Jquery", "Firebase", "Bootstrap"],
+        links: [
+          {
+            url: "https://cablelocator.net",
+            icon: "box-arrow-up-right",
+          },
+        ],
+        imgSrc: "assets/cablelocator.png",
+      },
+      {
+        title: "Featured Project",
+        subtitle: "Business website",
+        desc: "Website using Laravel, pure js as front-end with Bootstrap. Provide with admin dashboard and customizable themes.",
+        published_year: "2022",
+        languages: ["Laravel", "Jquery", "Bootstrap"],
+        links: [
+          {
+            url: "https://github.com/khinemyaezin/Jupitor",
+            icon: "github",
+          },
+          {
+            url: "https://khinemyaezin.com/Jupitor/",
+            icon: "box-arrow-up-right",
+          },
+        ],
+        imgSrc: "assets/business-website.png",
+      },
+      {
+        title: "Featured Project",
+        subtitle: "Ecommerce",
+        desc: `All in one place to create <span class="highlight">beautiful online stores</span>
           including dashboard and reports.`,
-          published_year: "2022",
-      
-          languages: ["Laravel", "Bootstrap", "Angular"],
-          links: [],
-          imgSrc: "assets/ecommerce.png",
-        },
-        {
-          title: "Featured Project",
-          subtitle: "Unicode Typing Master",
-          desc: `Developed during transitional period of <span class="highlight">Zawgyi to
+        published_year: "2022",
+        languages: ["Laravel", "Bootstrap", "Angular"],
+        links: [],
+        imgSrc: "assets/ecommerce.png",
+      },
+      {
+        title: "Featured Project",
+        subtitle: "Unicode Typing Master",
+        desc: `Developed during transitional period of <span class="highlight">Zawgyi to
           Unicode</span>.`,
-          published_year: "2020",
-      
-          languages: ["C#"],
-          links: [
-            {
-              url: "https://github.com/khinemyaezin/unicode-typing-master",
-              icon: "github",
-            },
-            {
-              url: "https://github.com/khinemyaezin/unicode-typing-master/tree/main/bin/Debug",
-              icon: "download",
-            },
-          ],
-          imgSrc: "assets/unicode-typing-master.png",
-        },
-      ];
+        published_year: "2020",
 
-    this.workingExp =  [
-        {
-          position: "Backend Developer",
-          companyName: "Yomabank",
-          companyURL: "https://yomabank.com/",
-          fromDate: "2023",
-          toDate: "present",
-          responsiblity: [
-            {
-              role: "Business Banking:",
-              description: [
-                "Developed micro-services, following industry best practices and design principles.",
-                "Collaborated closely with cross-functional teams, including product owners and quality assurance, to ensure the successful delivery of micro-service projects",
-              ],
-            },
-          ],
-        },
-        {
-          position: "Full Stack Developer",
-          companyName: "MIT",
-          companyURL: "https://www.mit.com.mm/",
-          fromDate: "2019",
-          toDate: "2021",
-          responsiblity: [
-            {
-              role: "Retail Point of Sale System Developer and Maintainer:",
-              description: [
-                "Developed and maintained a retail point of sale system, focusing on optimizing code complexity and enhancing processing time.",
-                "Implemented optimizations and enhancements to enhance the system's performance and user experience.",
-              ],
-            },
-            {
-              role: "Database Transaction Administration:",
-              description: [
-                "Took responsibility for the administration of database transaction issues at client sites, ensuring smooth and reliable data transactions",
-              ],
-            },
-            {
-              role: "Spring and Angular Developer:",
-              description: [
-                "Designed and executed efficient and high-performance retail, delivery, and survey applications, from the ground up.",
-                "Utilized Spring and Angular technologies to create robust and scalable solutions that met client needs.",
-              ],
-            },
-            {
-              role: "SAP Integration:",
-              description: [
-                "Integrated web application with SAP using BAPIs for efficient data exchange and synchronization.",
-              ],
-            },
-          ],
-        },
-      ];
+        languages: ["C#"],
+        links: [
+          {
+            url: "https://github.com/khinemyaezin/unicode-typing-master",
+            icon: "github",
+          },
+          {
+            url: "https://github.com/khinemyaezin/unicode-typing-master/tree/main/bin/Debug",
+            icon: "download",
+          },
+        ],
+        imgSrc: "assets/unicode-typing-master.png",
+      },
+    ];
+
+    this.workingExp = [
+      {
+        position: "Backend Developer",
+        companyName: "Yomabank",
+        companyURL: "https://yomabank.com/",
+        fromDate: "2023",
+        toDate: "present",
+        responsiblity: [
+          {
+            role: "Business Banking:",
+            description: [
+              "Optimized the user registration process of middleware service by implementing enhanced API design and utilizing a non-blocking request handling system, achieving a 50% reduction in registration time and significant enhancements in resource efficiency.",
+              "Standardized error handling across microservices by implementing a uniform exception-handling process, resulting in 100% consistency and reliability in error management throughout the system.",
+              "Built APIs for the bank operation dashboard project, sunsetting third-party service and resulting in million-dollar savings for the company.",
+              "Enhanced query optimization within the transaction service, leading to a 30% increase in data fetching speed for user transactions.",
+            ],
+          },
+        ],
+      },
+      {
+        position: "Developer",
+        companyName: "MIT",
+        companyURL: "https://www.mit.com.mm/",
+        fromDate: "2019",
+        toDate: "2021",
+        responsiblity: [
+          {
+            role: "Retail Point of Sale System Developer and Maintainer:",
+            description: [
+              "Led the end-to-end development of the retail project's backend service, serving a daily active clientele of over 5,000 local retailers across the region.",
+              "Developed an admin dashboard module for a delivery project, empowering customers to monitor delivery routes and track ordered items.",
+              "Implemented a feature in the surveyor application utilizing the Google Maps service to analyze surroundings and propose optimal merchandise placement based on context.",
+              "Contributed to seamlessly integrating accounting processes with SAP BAPIs, ensuring accurate data migration with minimal discrepancies.",
+            ],
+          },
+        ],
+      },
+    ];
   }
 
   init() {
@@ -136,7 +120,7 @@ class Portfolio {
       "Data Whisperer.",
       "Code Conjurer.",
       "Backend Enchanter.",
-      "Data Dynamo."
+      "Data Dynamo.",
     ]);
     this.prepareProjects();
     this.runAos();
@@ -199,7 +183,7 @@ class Portfolio {
   }
 
   resizeWindow() {
-    document.addEventListener('resize', this.space.resizeCanvas);
+    document.addEventListener("resize", this.space.resizeCanvas);
   }
 
   prepareProjects() {
@@ -289,11 +273,11 @@ class Portfolio {
                 .map(
                   (resp_value, resp_index) =>
                     `<ul>
-                    <li class="highlight">${resp_value.role}</li>
+                    
                     <li>${resp_value.description
                       .map(
                         (desc_value, desc_index) =>
-                          `<li data-aos-once="true" data-aos="zoom-out-left" data-aos-once="true" data-aos-delay="${
+                          `<li class='box-container mb-2' data-aos-once="true" data-aos="zoom-out-left" data-aos-once="true" data-aos-delay="${
                             init * desc_index
                           }">
                         <span class="indicator"></span>
