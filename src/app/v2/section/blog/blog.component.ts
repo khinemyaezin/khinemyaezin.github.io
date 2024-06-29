adde89cb1696e3eaf0aa640dde98d428139703dc
Khine Myae <50288473+khinemyaezin@users.noreply.github.com>
2024-06-29 15:11:42 +0900

    fix languages

--- src/app/v2/section/blog/blog.component.ts
+++ src/app/v2/section/blog/blog.component.ts
@@ -16,7 +16,7 @@ export class BlogComponent {
       title: "Senior Backend Developer",
       company:"@Yomabank",
       duration:"2023 - 2024",
-      languages: ["Javascript with Jquery", "Firebase", "Bootstrap"],
+      languages: ["Spring boot", "Microservices"],
       image:
         "https://upload.wikimedia.org/wikipedia/en/3/3c/Yoma_Bank_Logo.svg",
       desc:"Develop and create APIs used to construct for the bank, across the whole product. Work closely with cross-functional teams, including developers, designers, and product managers, to implement and advocate for best practices."
@@ -25,7 +25,7 @@ export class BlogComponent {
       title: "Developer",
       company:"@MIT",
       duration:"2019 - 2021",
-      languages: ["Javascript with Jquery", "Firebase", "Bootstrap"],
+      languages: ["Spring MVC","C#", "Angular", "Ionic"],
       image:
         "https://custom-images.strikinglycdn.com/res/hrscywv4p/image/upload/c_limit,fl_lossy,h_300,w_300,f_auto,q_auto/1113117/s5fcprkr6p67qbdyl2y0.png",
       desc:"Developed software solutions diverse array of projects such as Retail, Delivery,Survey and point-of-sale applications. Collaborated with other developers to integrate systems for the clients."
